d11cdc51920bbff9bf864431cbd266024a94d158
Aliaksandr Dubouski <sashok2000888@gmail.com>
2021-05-22 23:04:58 +0300

    Sonar(new)

--- src/main/java/Lesson1/CalkulatorTest.java
+++ src/main/java/Lesson1/CalkulatorTest.java
@@ -1,11 +1,14 @@
 package Lesson1;
 
+import static java.lang.System.*;
+
 class CalkulatorTest {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        var calculator = new Calculator();
+        int[] arr;
+        arr = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
         calculator.printArray(arr);
-        System.out.println();
+        out.println();
         calculator.printArray(calculator.arraySort(arr));
     }
 }
